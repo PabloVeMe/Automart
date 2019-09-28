3d51b3c40ddbc546c982b84e90161736ee6e8f4c
Pablo <velazquezm.pablo@gmail.com>
2019-09-27 19:19:35 -0500

    agregando imagen a objeto auto

--- main_app/views.py
+++ main_app/views.py
@@ -7,18 +7,20 @@ def index(request):
     return render(request, 'index.html', {'autos':autos})
 
 class Auto:
-    def __init__(self, nombre, modelo, precio, color):
+    def __init__(self, nombre, modelo, precio, color, img_url):
         self.nombre=nombre
         self.modelo=modelo
         self.precio=precio
         self.color=color
+        self.img_url=img_url
 
+image="https://img.motoryracing.com/noticias/portada/23000/23731-n.jpg"
 autos=[
-  Auto("VW jetta", 2018, 145500, "rojo")  
-  Auto("Futura", 1955, 0, "aqua")
-  Auto("Seat", 2001, 11000, "azul")
-  Auto("Ford", 1999, 34000, "negro")
+  Auto("VW jetta", 2018, 145500, "rojo", image),  
+  Auto("Futura", 1955, 0, "aqua", image), 
+  Auto("Seat", 2001, 11000, "azul", image), 
+  Auto("Ford", 1999, 34000, "negro", image), 
 
 ]
-a=Auto("VW jetta", 2018, 145500, "rojo")
+
 
